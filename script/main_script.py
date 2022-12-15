4ef3446b8f903c24c2eb0b62e093815557efdcf7
Edmund Stell <edmundstell@Edmunds-MacBook-Pro.local>
2022-12-15 16:48:36 +0000

    policy role created

--- script/main_script.py
+++ script/main_script.py
@@ -1,4 +1,4 @@
-from script.docs_script import cw_policy_json, s3_read_policy_json
+# from script.docs_script import cw_policy_json, s3_read_policy_json
 import zipfile
 import logging
 import boto3
@@ -7,9 +7,9 @@ import time
 import json
 
 timestamp = round(time.time())
-code_bucket = f'bucketname_{timestamp}'
-ingestion_bucket =f'raw_data_bucket_{timestamp}'
-processed_data_bucket = f'processed_data_bucket_{timestamp}'
+code_bucket = f'bucketname-{timestamp}'
+ingestion_bucket =f'raw-data-bucket-{timestamp}'
+processed_data_bucket = f'processed-data-bucket-{timestamp}'
 function_name = f'de_pyoneers_lambda_{timestamp}'
 aws_region = 'us-east-1'
 sts_client = boto3.client('sts')
@@ -29,11 +29,11 @@ def create_bucket(bucket_name, region='us-east-1'):
     return True
 
 def zipper(lambda_function_path, zip_file_name, bucket=code_bucket):
-    with zipfile.ZipFile(f'../{zip_file_name}', 'w') as zip:
+    with zipfile.ZipFile(f'./{zip_file_name}', 'w') as zip:
         zip.write(lambda_function_path)
     s3 = boto3.client('s3')
 
-    s3.upload_file(f'../{zip_file_name}', bucket, f'{function_name}/function.zip')
+    s3.upload_file(f'./{zip_file_name}', bucket, f'{function_name}/function.zip')
 
 def setting_iam_policies():
     # Create an IAM client
@@ -61,88 +61,149 @@ def setting_iam_policies2():
     iam = boto3.client('iam')
 
     CW_REGION = f"arn:aws:logs:us-east-1:{aws_user_id}:*"
-    CW_RES = f"arn:aws:logs:us-east-1:{aws_user_id}:log-group:/aws/lambda/{FUNCTION_NAME}:*"
+    CW_RES = f"arn:aws:logs:us-east-1:{aws_user_id}:log-group:/aws/lambda/{function_name}:*"
 
-# Read the CloudWatch log policy template
-    with open("../templates/cloudwatch_log_policy_template.json") as f:
-        policy_template = json.load(f)
+    with open("templates/cloudwatch_log_policy_template.json") as f:
+        cw_policy_template = json.load(f)
 
-# update the policy template with the ARNs
-    policy_template["Statement"][0]["Resource"] = CW_REGION
-    policy_template["Statement"][1]["Resource"] = CW_RES
+    cw_policy_template["Statement"][0]["Resource"] = CW_REGION
+    cw_policy_template["Statement"][1]["Resource"] = CW_RES
 
-
-
-    CODE_BUCKET_NAME = "code-bucket"
-    DATA_BUCKET_NAME = "data-bucket"
-
-    # Read the contents of the s3_read_policy_template.json file
-    with open("../templates/s3_read_policy_template.json", "r") as f:
+    with open("templates/s3_read_policy_template.json", "r") as f:
         S3_READ_POLICY_TEMPLATE = json.load(f)
 
-    # Update the .Statement[0].Resource elements in the policy template to refer to the specified S3 buckets
-    S3_READ_POLICY_TEMPLATE[".Statement"][0]["Resource"][0] = f"arn:aws:s3:::{DATA_BUCKET_NAME}/*"
-    S3_READ_POLICY_TEMPLATE[".Statement"][0]["Resource"][1] = f"arn:aws:s3:::{CODE_BUCKET_NAME}/*"
-    # update buckets variables
-    S3_READ_POLICY_TEMPLATE[".Statement"][0]["Resource"][2] = f"arn:aws:s3:::{BUCKETSSSSSS}/*"
+    S3_READ_POLICY_TEMPLATE["Statement"][0]["Resource"][0] = f"arn:aws:s3:::{code_bucket}/*"
+    S3_READ_POLICY_TEMPLATE["Statement"][0]["Resource"][1] = f"arn:aws:s3:::{ingestion_bucket}/*"
+    S3_READ_POLICY_TEMPLATE["Statement"][0]["Resource"][2] = f"arn:aws:s3:::{processed_data_bucket}/*"
 
-
-
-
-    # The Amazon Resource Name (ARN) of the AWS Lambda function 
-    lambda_function_arn = 'arn:aws:lambda:<REGION>:<ACCOUNT_ID>:function:<FUNCTION_NAME>'
-
-
-
-# Create a new target for the rule, which is the AWS Lambda function that you
-# want to be invoked when the rule is triggered
-eventbridge_client.put_targets(
-    Rule=rule_name,
-    Targets=[
-        {
-            'Id': 'my-eventbridge-target',
-            'Arn': lambda_function_arn
-        }
-    ]
-)
-
-
-
-    eventbridge = boto3.client('eventbridge')
-    lambda_client = boto3.client('lambda')
-
-    rule_name = 'OnFiveMinutes'
-
-# The schedule expression that determines how often the rule is triggered. In
-# this case, the rule will be triggered every five minutes
-    schedule_expression = 'rate(5 minutes)'
-    eventbridge.put_rule(
-    Name=rule_name,
-    ScheduleExpression=schedule_expression,
-    State='ENABLED'
+    s3_response = iam.create_policy(
+        PolicyName="s3_read_policy",
+        PolicyDocument=json.dumps(S3_READ_POLICY_TEMPLATE)
     )
 
-    lambda_client.add_permission(
-        FunctionName='FUNCTION_NAME',
-        StatementId='EVENTBRIDGE_INVOKE_1',
-        Action='lambda:InvokeFunction',
-        Principal='events.amazonaws.com',
-        SourceArn='arn:aws:events:REGION:ACCOUNT_ID:event-bus/EVENT_BUS_NAME'
+    s3_policy_arn = s3_response["Policy"]["Arn"]
+
+    cw_response = iam.create_policy(
+        PolicyName="cloudwatch_log_policy",
+        PolicyDocument=json.dumps(cw_policy_template)
     )
 
-    # Add the specified Lambda function as a target for the rule
-    eventbridge.put_targets(
-        Rule=rule_name,
-        Targets=[
-            {
-                'Id': 'EVENTBRIDGE_TARGET_1',
-                'Arn': lambda_function_arn
-            }
-        ]
+    cw_policy_arn = cw_response["Policy"]["Arn"]
+
+    with open('templates/trust_policy.json') as f:
+        trust_policy = json.load(f)
+
+    trust_policy_string = json.dumps(trust_policy)
+
+    response = iam.create_role(
+        RoleName=f"lambda-execution-role-{function_name}",
+        AssumeRolePolicyDocument=trust_policy_string
     )
+
+    EXECUTION_ROLE = response['Role']['Arn']
+
+    response2 = iam.attach_role_policy(
+        PolicyArn=s3_policy_arn,
+        RoleName="lambda-execution-role-{}".format(function_name)
+    )
+
+    iam.attach_role_policy(
+        PolicyArn=cw_policy_arn,
+        RoleName="lambda-execution-role-{}".format(function_name)
+    )
+
+    return {"response": response, "response2": response2}
+
+
+    # lambda_function_arn = f'arn:aws:lambda:{aws_region}:{aws_account}:function:{function_name}'
+
+    # lambda_client = boto3.client('lambda')
+
+
+
+
+
+    # eventbridge = boto3.client('events')
+
+
+    # rule_name = 'OnFiveMinutes'
+
+    # # The schedule expression that determines how often the rule is triggered. In
+    # # this case, the rule will be triggered every five minutes
+    # schedule_expression = 'rate(5 minutes)'
+    
+    # eventbridge.put_rule(
+    #     Name=rule_name,
+    #     ScheduleExpression=schedule_expression,
+    #     State='ENABLED'
+    # )
+
+    # lambda_client.add_permission(
+    #     FunctionName=f'{function_name}',
+    #     StatementId='EVENTBRIDGE_INVOKE_1',
+    #     Action='lambda:InvokeFunction',
+    #     Principal='events.amazonaws.com',
+    #     SourceArn=f'arn:aws:events:{aws_region}:{aws_account}:event-bus/my-event-bus'
+    # )
+
+    # # Add the specified Lambda function as a target for the rule
+    # eventbridge.put_targets(
+    #     Rule=rule_name,
+    #     Targets=[
+    #         {
+    #             'Id': 'EVENTBRIDGE_TARGET_1',
+    #             'Arn': lambda_function_arn
+    #         }
+    #     ]
+    # )
 
  
-    response = iam.attach_role_policy(
-        PolicyArn=S3_READ_POLICY_TEMPLATE,
-        RoleName="lambda-execution-role-{}".format(FUNCTION_NAME)
+    # response = iam.attach_role_policy(
+    #     PolicyArn=S3_READ_POLICY_TEMPLATE,
+    #     RoleName="lambda-execution-role-{}".format(function_name)
+    # )
+
+def create_lambda_function():
+    lambda_client = boto3.client('lambda')
+
+
+    iam_client = boto3.client('iam')
+
+    with open('templates/trust_policy.json') as f:
+        trust_policy = json.load(f)
+
+    trust_policy_string = json.dumps(trust_policy)
+
+    response = iam_client.create_role(
+        RoleName="lambda-execution-role-de_pyoneers_lambda_1671112825",
+        AssumeRolePolicyDocument=trust_policy_string
     )
+
+    EXECUTION_ROLE = response['Role']['Arn']
+
+    # Role=EXECUTION_ROLE,
+
+    response = lambda_client.create_function(
+        FunctionName=function_name,
+        Runtime='python3.9',
+        Role= EXECUTION_ROLE,
+        Handler='main.handler',
+        Code={
+            # 'ZipFile': open(deployment_package, 'rb').read(),
+            'S3Bucket': "bucketname-1671112825",
+            'S3Key': "de_pyoneers_lambda_1671112825/function.zip"
+        }
+    )
+
+    with open('confirmation.json', 'w') as f:
+        json.dump(response, f)
+
+# create_lambda_function()
+
+
+
+# create_bucket(code_bucket)
+# create_bucket(ingestion_bucket)
+# create_bucket(processed_data_bucket)
+# zipper('./script/test/test_lambda_function.py', 'function.zip', 'bucketname-1671112825')
+# setting_iam_policies2()
--- script/test/test_main_script.py
+++ script/test/test_main_script.py
@@ -1,16 +1,17 @@
 from unittest.mock import Mock, patch
 import pytest
-from script.main_script import create_bucket, zipper, function_name, setting_iam_policies
-from moto import mock_s3
+from script.main_script import create_bucket, zipper, function_name, setting_iam_policies2
+from moto import mock_s3, mock_iam
 import time
 import boto3
+from unittest.mock import patch
 
 
 @mock_s3
 def test_create_bucket():
     m3 = boto3.client('s3')
     timestamp = round(time.time())
-    bucket_name = f'bucketname_{timestamp}'
+    bucket_name = f'bucketname-{timestamp}'
     create_bucket(bucket_name)
     response =m3.list_buckets()
     assert bucket_name in [buckets['Name'] for buckets in response['Buckets']]
@@ -20,14 +21,45 @@ def test_create_bucket():
 def test_zipper():
     m3 = boto3.client('s3')
     m3.create_bucket(Bucket='my_bucket')
-    zipper('./test/test_lambda_function.py', 'function.zip', 'my_bucket')
+    zipper('script/test/test_lambda_function.py', 'function.zip', 'my_bucket')
     print(m3.list_objects(Bucket='my_bucket'))
     assert f'{function_name}/function.zip' in m3.list_objects(Bucket='my_bucket')['Contents'][0]['Key']
 
-@mock_s3
-def test_setting_iam_policies():
-    m3 = boto3.client('iam')
-    setting_iam_policies()
-    m3.list_attached_policies(UserName='username')
-    assert 1==2
+# @mock_iam
+# def test_setting_iam_policies2():
+#     m3 = boto3.client('iam')
+#     setting_iam_policies2()
+#     m3.list_attached_policies(UserName='username')
+#     assert 1==2
 
+@mock_iam
+def test_setting_iam_policies3():
+    with patch('script.main_script.function_name', new = 'test'):
+        iam = boto3.client('iam')
+        response = setting_iam_policies2()
+        print(repsonse)
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+        assert response['Role']['Arn'] == f'arn:aws:iam::123456789012:role/lambda-execution-role-test'
+        assert iam.attach_role_policy(PolicyArn=s3_policy_arn, RoleName="lambda-execution-role-{}".format(function_name))
+        assert iam.assert_called_with(PolicyArn=cw_policy_arn, RoleName="lambda-execution-role-{}".format(function_name))
+
+
+from moto import mock_iam
+
+@mock_iam
+def test_setting_iam_policies4():
+    iam = boto3.client('iam')
+    response = setting_iam_policies2()
+
+    # Get the attach_role_policy method from the mock iam object
+    attach_role_policy_method = iam.attach_role_policy
+
+    print(dir(attach_role_policy_method))
+
+    # Get the policy ARNs that were passed to the attach_role_policy method
+    s3_policy_arn = attach_role_policy_method.call_args_list[0][1]['PolicyArn']
+    cw_policy_arn = attach_role_policy_method.call_args_list[1][1]['PolicyArn']
+
+    # Assert that the attach_role_policy method was called with the correct policy ARNs
+    assert attach_role_policy_method.assert_called_with(PolicyArn=s3_policy_arn, RoleName="lambda-execution-role-{}".format(function_name))
+    assert attach_role_policy_method.assert_called_with(PolicyArn=cw_policy_arn, RoleName="lambda-execution-role-{}".format(function_name))
