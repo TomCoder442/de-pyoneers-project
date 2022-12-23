d9f332b2667e5d2dbdde465ae77e78506067285b
Joseph Rigby <josephrigby@Josephs-MacBook-Pro-2.local>
2022-12-23 15:33:31 +0000

    formatting changes, still runs well

--- deployment/test/test_lambda_function.py
+++ deployment/test/test_lambda_function.py
@@ -1,20 +1,21 @@
 import json
 import pandas as pd
-import awswrangler as wr
-import boto3
+# import awswrangler as wr
+# import boto3
 
 
-def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket("pyoneers-extracted-data-test-2022-12-20-22")
-    for obj in bucket.objects.all():
-        key = obj.key
-        print(key)
-        body = obj.get()['Body']
+# def lambda_handler(event, context):
+#     s3 = boto3.resource('s3')
+#     bucket = s3.Bucket("pyoneers-extracted-data-test-2022-12-20-22")
+#     for obj in bucket.objects.all():
+#         key = obj.key
+#         print(key)
+#         body = obj.get()['Body']
         
-        s3_url = f's3://pyoneers-processed-data-test-2022-12-20-22/{key}.parquet'
+#         s3_url = f's3://pyoneers-processed-data-test-2022-12-20-22/{key}.parquet'
 
-        wr.s3.to_parquet(
-        df = pd.read_csv(body),
-        path=s3_url)
+#         wr.s3.to_parquet(
+#         df = pd.read_csv(body),
+#         path=s3_url)
     
+print("hello world")
