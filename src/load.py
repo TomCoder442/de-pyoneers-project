8870ef4761dd4b1991fb26d0dd4a34d7ac331e30
Edmund Stell <edmundstell@Edmunds-MacBook-Pro.local>
2023-01-03 16:39:25 +0000

    load and transform patched to fix serial primary key error

--- src/load.py
+++ src/load.py
@@ -30,96 +30,9 @@ def lambda_handler():
         s3 = boto3.resource('s3')
         object = s3.Object('processed-data-bucket-2022-12-21-1617', tbl_parquet)
         object.download_fileobj(buffer)
-        conn = engine.connect()
-        result = conn.execute(f'SELECT * FROM {table_name}')
-        database_data = pd.DataFrame(result.fetchall())
         df = pd.read_parquet(buffer)
 
-        unique_key_columns = {
-            'dim_counterparty': 'counterparty_id',
-            'dim_currency': 'currency_id',
-            'dim_date': 'date_id',
-            'dim_design': 'design_id',
-            'dim_location': 'location_id',
-            'dim_payment_type': 'payment_type_id',
-            'dim_staff': 'staff_id',
-            'dim_transaction': 'transaction_id',
-            'fact_payment': 'payment_record_id',
-            'fact_purchase_order': 'purchase_record_id',
-            'fact_sales_order': 'sales_record_id',
-        }
-
-        if database_data.empty:
-            df.to_sql(table_name, engine, index=False, if_exists='append')
-        else:
-            unique_key_column = unique_key_columns[table_name]
-            if table_name == 'dim_date':
-                # df[unique_key_column] = pd.to_datetime(df[unique_key_column])
-                database_data[unique_key_column] = pd.to_datetime(database_data[unique_key_column])
-
-            # Merge df and database_data and add an indicator column
-            print(df)
-            print('hi')
-            print(database_data)
-            merged_data = pd.merge(df, database_data, on=[unique_key_column], how='outer', indicator='duplicate', suffixes=['', '_database'])
-            print(merged_data)
-
-            # Keep only the rows that are unique to df
-            unique_data = merged_data[merged_data['duplicate'] == 'left_only']
-
-            # Drop the indicator column and insert the resulting data into the table in the database
-            unique_data = unique_data.drop(columns=['duplicate'])
-            print('yo')
-            print(unique_data)
-            if table_name== 'dim_transaction':
-                    unique_data['sales_order_id'] = unique_data['sales_order_id'].fillna(0)
-                    unique_data['purchase_order_id'] = unique_data['purchase_order_id'].fillna(0)
-                    unique_data['sales_order_id'] = unique_data['sales_order_id'].astype(int)
-                    unique_data['purchase_order_id'] = unique_data['purchase_order_id'].astype(int)
-            if table_name== 'fact_purchase_order':
-                    unique_data['purchase_record_id'] = unique_data['purchase_record_id'].astype(int)
-                    unique_data['staff_id'] = unique_data['staff_id'].astype(int)
-                    unique_data['counterparty_id'] = unique_data['counterparty_id'].astype(int)
-                    unique_data['item_quantity'] = unique_data['item_quantity'].astype(int)
-                    unique_data['currency_id'] = unique_data['currency_id'].astype(int)
-                    unique_data['agreed_delivery_location_id'] = unique_data['agreed_delivery_location_id'].astype(int)
-            if table_name == 'fact_sales_order':
-                    unique_data['sales_record_id'] = unique_data['sales_record_id'].astype(int)
-                    unique_data['sales_staff_id'] = unique_data['sales_staff_id'].astype(int)
-                    unique_data['counterparty_id'] = unique_data['counterparty_id'].astype(int)
-                    unique_data['units_sold'] = unique_data['units_sold'].astype(int)
-                    unique_data['currency_id'] = unique_data['currency_id'].astype(int)
-                    unique_data['design_id'] = unique_data['design_id'].astype(int)
-                    unique_data['agreed_delivery_location_id'] = unique_data['agreed_delivery_location_id'].astype(int)
-                    
-            
-            for row in unique_data.iterrows():
-                # Get the column names and values as a list
-                columns = df.columns
-                values = [f"'{row[column]}'" for column in columns]
-                # Convert the list of values to a string with comma-separated values
-                values_str = ", ".join(values)
-                
-                insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({values_str})"
-                print(insert_query)
-                engine.execute(insert_query)
+        df.to_sql(table_name, engine, index=False, if_exists='append')
 
         engine.execute(f'SELECT * FROM {table_name} limit 5').fetchall()
     list_files()
-
-
-            # for index, row in unique_data.iterrows():
-            #     columns = df.columns
-            #     set_clause = ""
-            #     # Iterate through the column names and add the column name and value to the SET clause
-            #     for column in columns:
-            #         set_clause += f"{column} = '{row[column]}', "
-            #     # Remove the last comma and space from the SET clause
-            #     set_clause = set_clause[:-2]
-                
-            #     update_query = f"UPDATE {table_name} SET {set_clause} WHERE {unique_key_column} = '{row[unique_key_column]}'"
-            #     print(update_query)
-            #     engine.execute(update_query)
-
-
-
